67ba6b5d24cc43a73ba156c785218bb886ac1c67
matheusmarazzi <azevedomarazzi@gmail.com>
2023-07-23 21:43:35 -0300

    deploy commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Backend do projeto pizzaria do curso de typeScript
+# deployPizzaria
+# deployPizzaria
---
+++ src/services/product/ListByCategoryService.ts
@@ -0,0 +1,18 @@
+import prismaClient from "../../prisma";
+
+interface ProductResquest{
+    category_id: string;
+}
+
+class ListByCategotyService{
+    async execute({category_id}: ProductResquest){
+        const findByCategory = await prismaClient.product.findMany({
+            where:{
+                category_id:category_id
+            }
+        })
+        return findByCategory;
+    }   
+}
+
+export {ListByCategotyService}
